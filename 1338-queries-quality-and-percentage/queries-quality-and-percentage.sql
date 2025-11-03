d35e232e52d28b143ff02d8682a4f9556d0fa97f
Rawan Gaber <116557235+rawan-sleem@users.noreply.github.com>
2025-11-03 08:50:14 +0200

    Time: 927 ms (68.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,16 @@
+/* Write your T-SQL query statement below */
+
+
+SELECT 
+    query_name,
+    ROUND(
+        AVG(1.0 * rating / position), 2) AS quality,  
+    ROUND(
+        100.0 * SUM(
+            CASE WHEN rating < 3 THEN 1 ELSE 0 END
+            ) /
+             COUNT(*),
+        2
+    ) AS poor_query_percentage  
+FROM Queries
+GROUP BY query_name;
